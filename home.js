f7297ed33034ab935eebdb0273ed21bfa18ece9a
Tshiamo-RicheeyM <richeey4@gmail.com>
2025-06-27 21:56:49 +0200

    index

---
+++ home.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const hero = document.querySelector('.hero-content');
+  hero.style.opacity = 1;
+  hero.style.transform = 'translateY(0)';
+});

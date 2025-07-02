ca0306dae7835661a03b5dd65765f272983df437
Tshiamo-RicheeyM <richeey4@gmail.com>
2025-07-02 14:33:23 +0200

    contact js

---
+++ contact.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert("This form is currently inactive.\nPlease contact us via WhatsApp or visit in person.");
+});
